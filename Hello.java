234dfbc7f21d825ee04ca079e40302195a835e9c
SACHIN SUNIL JAGTAP <103174978+sachin-sunil-jagtap@users.noreply.github.com>
2022-05-21 20:10:38 +0530

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,14 +1,14 @@
 // Below statement is used to create the package named as Marvellous
 package Marvellous;
 
-public class Demo
+public class Hello
 {
-    public void fun()
+    public void gun()
     {
-        System.out.println("Inside fun of Demo");
+        System.out.println("Inside gun of Hello");
     }
 }
 
-// javac Demo.java  -d .
+// javac Hello.java -d .
 // -d       Directory
 // .        Current Directory
---
+++ PPA.java
@@ -0,0 +1,18 @@
+// Below statements are used to import the package
+import Marvellous.Demo;
+import Marvellous.Hello;
+
+class PPA
+{
+    public static void main(String arg[])
+    {
+        Demo dobj = new Demo();
+        Hello hobj = new Hello();
+
+        dobj.fun();
+        hobj.gun();
+    }
+}
+
+// javac PPA.java
+// java PPA
